db8fd01006fd85949eca366aecb35ba2ea4f4240
Ouyancheng <1024842937@qq.com>
2022-05-15 18:59:46 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RaspberryPi4-baremetal
+
+# Reference 
+
+https://github.com/isometimes/rpi4-osdev 
+
+
+
+
+
--- libpipi/mbox.h
+++ libpipi/mbox.h
@@ -1,3 +1,8 @@
+/**
+ * I am too lazy to dig into bcm2711's documentation (it should be largely similar to bcm2835 though), 
+ * so I just transcribe the code from the following amazing repository: 
+ * Reference: https://github.com/isometimes/rpi4-osdev/blob/master/part5-framebuffer/mb.h 
+ */
 #pragma once 
 #ifdef __cplusplus
 extern "C" {
@@ -21,8 +26,13 @@ extern volatile unsigned int mbox[36];
 // tags
 #define MBOX_TAG_GETSERIAL      0x10004
 #define MBOX_TAG_SETCLKRATE     0x38002
-#define MBOX_TAG_LAST           0
-
+#define MBOX_TAG_SETPHYWH       0x48003 
+#define MBOX_TAG_SETVIRTWH      0x48004 
+#define MBOX_TAG_SETVIRTOFF     0x48009 
+#define MBOX_TAG_SETDEPTH       0x48005 
+#define MBOX_TAG_SETPXLORDR     0x48006 
+#define MBOX_TAG_GETFB          0x40001 
+#define MBOX_TAG_GETPITCH       0x40008 
 int mbox_call(unsigned char ch);
 
 #ifdef __cplusplus
