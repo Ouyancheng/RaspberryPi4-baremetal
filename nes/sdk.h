b2ab05667381e7dd276fa3312ece9530e9fb1553
Ouyancheng <1024842937@qq.com>
2022-05-29 12:30:05 -0700

    minor cleanup

--- nes/sdk.h
+++ nes/sdk.h
@@ -35,19 +35,14 @@ static inline unsigned get_current_time_ms(void) {
 #ifdef PLATFORM_PC  
 #   include <string.h>
 #   include <stdio.h>
-// #   include <unistd.h>
 #   include <stdlib.h>
 #   include <signal.h>
 #   include <SDL2/SDL.h>
-// static inline void delay_us(unsigned us) {
-//     usleep(us); 
-// }
+
 static inline void delay_ms(unsigned ms) {
-    // delay_us(ms * 1000); 
     SDL_Delay(ms); 
 }
 static inline void delay_sec(unsigned sec) {
-    // sleep(sec); 
     for (unsigned i = 0; i < sec; ++i) {
         delay_ms(1000); 
     }
@@ -57,13 +52,9 @@ static inline unsigned get_current_time_ms(void) {
 }
 
 #endif 
-
-
-
 int panic_at(const char *file, int line, const char *function, const char *format, ...); 
 #define panic(...) panic_at(__FILE__, __LINE__, __func__, __VA_ARGS__)
 #define assert(condition) ((condition) ? ((void)0) : ((void)panic("assertion " #condition " failed"))) 
-
 
 
 struct rgb {
@@ -72,12 +63,6 @@ struct rgb {
     uint8_t g; 
     uint8_t b; 
 };
-
-
-
-
-
-
 static inline uint8_t set_mask(uint8_t reg, uint8_t mask) {
     reg |= mask; 
     return reg; 
@@ -89,6 +74,8 @@ static inline uint8_t clear_mask(uint8_t reg, uint8_t mask) {
 #define set_mask_inplace(reg, mask) ((reg) |= ((uint8_t)(mask)))
 #define clear_mask_inplace(reg, mask) ((reg) &= (~((uint8_t)(mask))))
 void do_nothing(void);
+
+
 
 unsigned load_file(uint8_t *target, unsigned target_size, const char *filename); 
 
